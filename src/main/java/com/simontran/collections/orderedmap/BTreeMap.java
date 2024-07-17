d9b2d36260d41d0a1238114719e9aa2a23e6454f
Simon Tran <simontran103@gmail.com>
2024-07-16 23:19:58 -0400

    quick save

--- src/main/java/com/simontran/collections/orderedmap/BTreeMap.java
+++ src/main/java/com/simontran/collections/orderedmap/BTreeMap.java
@@ -1,4 +1,31 @@
 package com.simontran.collections.orderedmap;
 
-public class BTreeMap {
+public class BTreeMap<K extends Comparable<K>, V> implements OrderedMap<K, V> {
+    public K min() {
+        return null;
+    }
+
+    public K max() {
+        return null;
+    }
+
+    public K successor(K key) {
+        return null;
+    }
+
+    public K predecessor(K key) {
+        return null;
+    }
+
+    public V get(K key) {
+        return null;
+    }
+
+    public void insert(K key, V value) {
+
+    }
+
+    public void remove(K key) {
+
+    }
 }
